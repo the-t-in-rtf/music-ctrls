5292306f9dc9f27be9a78a3ac48cc08e1b80934f
Dinuka De Silva <dinuka.2013513@iit.ac.lk>
2016-05-22 22:43:52 +0530

    Code Refactoring

--- test/spec/knob.spec.js
+++ test/spec/knob.spec.js
@@ -1,52 +1,57 @@
-(function(fluid, QUnit) {
+(function(fluid, jqUnit) {
     "use strict";
 
-    QUnit.test("basics of ring slider", function( assert ) {
-        var done = assert.async();
+    fluid.registerNamespace("fluid.test.knob");
+
+    fluid.test.knob.verifyStyles = function(knob, properties) {
+        jqUnit.assertEquals("value '" + properties.value + "' should have been applied to the slider",
+            properties.value + "%",
+            knob.locate("valueLabel").text()
+        );
+        jqUnit.assertEquals("text color '" + properties.color + "' should have been applied to the slider",
+            properties.color,
+            fluid.test.helpers.toHex(knob.locate("valueLabel").css("fill"))
+        );
+        jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
+            properties.color,
+            fluid.test.helpers.toHex(knob.locate("valueRing").css("stroke"))
+        );
+        jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
+            properties.color,
+            fluid.test.helpers.toHex(knob.locate("valueRingCover").css("stroke"))
+        );
+        jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
+            properties.color,
+            fluid.test.helpers.toHex(knob.locate("valueRing").css("fill"))
+        );
+        jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
+            properties.color,
+            fluid.test.helpers.toHex(knob.locate("valueRingCover").css("fill"))
+        );
+    };
+    jqUnit.test("basics of ring slider", function() {
         var elm = $("<div class='test1' style='width:100px; height: 150px'></div>");
         $("body").append(elm);
-        assert.equal(elm.html().length, 0, "Element should be empty before the evaluation");
+        var knob = fluid.sisiliano.knob(".test1");
 
-        fluid.sisiliano.knob(".test1");
-
-        var defaultColor = "#009688";
-        var defaultValue = "0%";
-
-        setTimeout(function() {
-            assert.notEqual(elm.html().length, 0, "Ring slider should be appended to the element in a while");
-            assert.equal(elm.find(".ctrl-circle-value").text(), defaultValue, "Default value '" + defaultValue + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-value").css("fill")), defaultColor, "Default text color '" + defaultColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-cover").css("stroke")), defaultColor, "Default slider color '" + defaultColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-background").css("stroke")), defaultColor, "Default slider color '" + defaultColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-cover").css("fill")), defaultColor, "Default slider color '" + defaultColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-background").css("fill")), defaultColor, "Default slider color '" + defaultColor + "' should have been applied to the slider");
-            done();
-        }, 10);
+        fluid.test.knob.verifyStyles(knob, {
+            color: "#009688",
+            value: "0"
+        });
     });
 
-    QUnit.test("Custimizations of ring slider", function( assert ) {
-        var done = assert.async();
+    jqUnit.test("Custimizations of ring slider", function() {
         var elm = $("<div class='test2' style='width:100px; height: 150px'></div>");
         $("body").append(elm);
-        assert.equal(elm.html().length, 0, "Element should be empty before the evaluation");
 
-        fluid.sisiliano.knob(".test2", {
+        var knob = fluid.sisiliano.knob(".test2", {
             value: 30,
             color: "blue"
         });
 
-        var customizedColor = "#0000FF";
-        var customizedValue = "30%";
-
-        setTimeout(function() {
-            assert.notEqual(elm.html().length, 0, "Ring slider should be appended to the element in a while");
-            assert.equal(elm.find(".ctrl-circle-value").text(), customizedValue, "Customized value '" + customizedValue + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-value").css("fill")), customizedColor, "Customized text color '" + customizedColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-cover").css("stroke")), customizedColor, "Customized slider color '" + customizedColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-background").css("stroke")), customizedColor, "Customized slider color '" + customizedColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-cover").css("fill")), customizedColor, "Customized slider color '" + customizedColor + "' should have been applied to the slider");
-            assert.equal(fluid.test.helpers.toHex(elm.find(".ctrl-circle-background").css("fill")), customizedColor, "Customized slider color '" + customizedColor + "' should have been applied to the slider");
-            done();
-        }, 10);
+        fluid.test.knob.verifyStyles(knob, {
+            value: 30,
+            color: "blue"
+        });
     });
-})(fluid, QUnit);
+})(fluid, jqUnit);
