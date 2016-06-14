331771628e28d79773f2bef1768933ae2ceecc9d
Dinuka De Silva <dinuka.2013513@iit.ac.lk>
2016-06-14 14:04:39 +0530

    Added test cases for key events in the knob (This is failing in pantomJS)

--- test/spec/knob.spec.js
+++ test/spec/knob.spec.js
@@ -1,9 +1,35 @@
-(function(fluid, jqUnit) {
+(function (fluid, jqUnit) {
     "use strict";
 
     fluid.registerNamespace("fluid.test.knob");
 
-    fluid.test.knob.verifyStyles = function(knob, properties) {
+    fluid.test.knob.getElementNode = function (knob) {
+        return d3.select(knob.container.selector).node();
+    };
+
+    //Verifying styles and inouts
+    jqUnit.test("knob: default styles and inputs", function () {
+        var knob = fluid.sisiliano.knob(".test");
+
+        fluid.test.knob.verifyStyles(knob, {
+            color: "#009688",
+            value: "0"
+        });
+    });
+
+    jqUnit.test("knob: customized styles and inputs", function () {
+        var knob = fluid.sisiliano.knob(".test", {
+            value: 30,
+            color: "#0000BB"
+        });
+
+        fluid.test.knob.verifyStyles(knob, {
+            value: 30,
+            color: "#0000BB"
+        });
+    });
+
+    fluid.test.knob.verifyStyles = function (knob, properties) {
         jqUnit.assertEquals("value '" + properties.value + "' should have been applied to the slider",
             properties.value + "%",
             knob.locate("valueLabel").text()
@@ -14,7 +40,7 @@
         );
         jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
             properties.color,
-            fluid.test.helpers.toHex(knob.locate("valueRing").css("stroke"))
+            fluid.test.helpers.toHex(knob.locate("valueCircle").css("stroke"))
         );
         jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
             properties.color,
@@ -22,36 +48,55 @@
         );
         jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
             properties.color,
-            fluid.test.helpers.toHex(knob.locate("valueRing").css("fill"))
+            fluid.test.helpers.toHex(knob.locate("valueCircle").css("fill"))
         );
         jqUnit.assertEquals("slider color '" + properties.color + "' should have been applied to the slider",
             properties.color,
             fluid.test.helpers.toHex(knob.locate("knobBackgroundCircle").css("fill"))
         );
     };
-    jqUnit.test("basics of ring slider", function() {
-        var elm = $("<div class='test1' style='width:100px; height: 150px'></div>");
-        $("body").append(elm);
-        var knob = fluid.sisiliano.knob(".test1");
 
-        fluid.test.knob.verifyStyles(knob, {
-            color: "#009688",
-            value: "0"
+    //Verifying key events
+    jqUnit.test("knob : key events", function () {
+        var knob = fluid.sisiliano.knob(".test");
+
+        fluid.test.knob.verifyKeyEvents(knob);
+    });
+    fluid.test.knob.verifyKeyEvents = function (knob) {
+        var expectedValue = knob.model.value + 1;
+        fluid.test.knob.pressKeyUp(knob);
+        jqUnit.assertEquals("Value should be increased when up key is pressed", expectedValue, knob.model.value);
+
+        expectedValue = knob.model.value - 1;
+        fluid.test.knob.pressKeyDown(knob);
+        jqUnit.assertEquals("Value should be decreased when up down is pressed", expectedValue, knob.model.value);
+    };
+
+    fluid.test.knob.pressKeyUp = function (knob) {
+        fluid.test.knob.triggerKeyDown(knob, 38);
+    };
+    fluid.test.knob.pressKeyDown = function (knob) {
+        fluid.test.knob.triggerKeyDown(knob, 40);
+    };
+    fluid.test.knob.triggerKeyDown = function (knob, keyCode) {
+        var evt = new MouseEvent("keydown", {
+            view: window
         });
+        evt.keyCode = keyCode;
+        fluid.test.knob.triggerEvent(knob, evt);
+    };
+    fluid.test.knob.triggerEvent = function (knob, evt) {
+        d3.select(knob.container.selector).node().dispatchEvent(evt);
+    };
+
+    //Verifying mouse events
+    /*jqUnit.test("knob : mouse events", function () {
+        var knob = fluid.sisiliano.knob(".test");
+
+        fluid.test.knob.verifyMouseEvents(knob);
     });
 
-    jqUnit.test("Custimizations of ring slider", function() {
-        var elm = $("<div class='test2' style='width:100px; height: 150px'></div>");
-        $("body").append(elm);
+    fluid.test.knob.verifyMouseEvents = function () {
 
-        var knob = fluid.sisiliano.knob(".test2", {
-            value: 30,
-            color: "#0000BB"
-        });
-
-        fluid.test.knob.verifyStyles(knob, {
-            value: 30,
-            color: "#0000BB"
-        });
-    });
+    };*/
 })(fluid, jqUnit);
--- test/spec/piano.js
+++ test/spec/piano.js
@@ -3,33 +3,23 @@
 
     QUnit.test("basics of piano", function(assert) {
         var done = assert.async();
-        var elm = $("<div class='test1' style='width:100px; height: 150px'></div>");
-        $(".test1").remove();
-        $("body").append(elm);
-        assert.equal(elm.html().length, 0, "Element should be empty before the evaluation");
 
-        fluid.sisiliano.piano(".test1");
-
+        fluid.sisiliano.piano(".test");
         setTimeout(function() {
-            assert.notEqual(elm.html().length, 0, "Piano should be appended to the element in a while");
+            assert.notEqual($(".test").html().length, 0, "Piano should be appended to the element in a while");
             done();
         }, 2);
     });
 
     QUnit.test("Custimizations of piano", function(assert) {
         var done = assert.async();
-        var elm = $("<div class='test2' style='width:100px; height: 150px'></div>");
-        $(".test2").remove();
-        $("body").append(elm);
-        assert.equal(elm.html().length, 0, "Element should be empty before the evaluation");
 
-        fluid.sisiliano.piano(".test2", {
+        fluid.sisiliano.piano(".test", {
             value: 30,
             color: "blue"
         });
-
         setTimeout(function() {
-            assert.notEqual(elm.html().length, 0, "Piano should be appended to the element in a while");
+            assert.notEqual($(".test").html().length, 0, "Piano should be appended to the element in a while");
             done();
         }, 2);
     });
