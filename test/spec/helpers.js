4a85f226e63e969ee3b14291aa1ccdd5d59950f3
Dinuka De Silva <dinuka.2013513@iit.ac.lk>
2016-06-28 14:10:49 +0530

    Corrected the namespace of the test cases to "sisiliano.tests"

--- test/spec/helpers.js
+++ test/spec/helpers.js
@@ -1,14 +1,14 @@
 (function() {
     "use strict";
 
-    fluid.registerNamespace("sisiliano.helpers");
+    fluid.registerNamespace("sisiliano.tests.helpers");
 
-    sisiliano.helpers.toHex = function(colorCode) {
+    sisiliano.tests.helpers.toHex = function(colorCode) {
         if (colorCode.indexOf("(") > 0) {
             var vals = colorCode.replace(/rgb|\(|\)/g, "").split(/ *,* /g);
 
             if (vals.length === 3) {
-                return "#" + sisiliano.helpers.dec2hex(vals[0]) + sisiliano.helpers.dec2hex(vals[1]) + sisiliano.helpers.dec2hex(vals[2]);
+                return "#" + sisiliano.tests.helpers.dec2hex(vals[0]) + sisiliano.tests.helpers.dec2hex(vals[1]) + sisiliano.tests.helpers.dec2hex(vals[2]);
             } else {
                 return colorCode;
             }
@@ -16,7 +16,7 @@
         return colorCode.toUpperCase();
     };
 
-    sisiliano.helpers.dec2hex = function(i) {
+    sisiliano.tests.helpers.dec2hex = function(i) {
         return (parseInt(i, "") + 0x10000).toString(16).substr(-2).toUpperCase();
     };
 })(fluid);
